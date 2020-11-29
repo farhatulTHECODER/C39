68a0d00455e093dcb5c0ca79dd29f91b22061ad2
unknown <skasraf001@gmail.com>
2020-11-29 16:38:54 +0530

    marked the player

--- js/Form.js
+++ js/Form.js
@@ -5,6 +5,7 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.rButton = createButton('Reset');
   }
   hide(){
     this.greeting.hide();
@@ -19,6 +20,7 @@ class Form {
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.rButton.position(displayWidth - 100 , 20);
 2
     this.button.mousePressed(()=>{
       this.input.hide();
@@ -31,6 +33,11 @@ class Form {
       this.greeting.html("Hello " + player.name)
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
+
+    this.rButton.mousePressed(()=>{
+      game.update(0);
+      player.updateCount(0);
+    })
 
   }
 }
